9f790c199b786fe3b1b63b2883633a97f044f4ba
lzc <30752740@qq.com>
2020-04-27 11:20:22 +0800

    更新

--- src/main/scala/com/atguigu/scala1128/day06/implicitdemo/ImplicitDemo5.scala
+++ src/main/scala/com/atguigu/scala1128/day06/implicitdemo/ImplicitDemo5.scala
@@ -1,7 +1,20 @@
 package com.atguigu.scala1128.day06.implicitdemo
 
 object ImplicitDemo5 {
-    def main(args: Array[String]): Unit = {
+    implicit val aaa: Int = 100
+    implicit val bbb: String = "abc"
     
+    
+    
+    def main(args: Array[String]): Unit = {
+        foo("利好")
+    }
+    
+    // a是隐式参数, 将来调用的时候根据需要,可以不传
+    // 让scala自动帮助我们传递, 找一个隐式值
+    def foo(s: String)(implicit a: Int, b:String) = {
+        println("s")
+        println(a)
+        println(b)
     }
 }
