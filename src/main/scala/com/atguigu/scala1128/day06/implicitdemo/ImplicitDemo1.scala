3837c2039736aec2e405470b8e3cffee2d7735cd
lzc <30752740@qq.com>
2020-04-27 09:20:06 +0800

    更新

---
+++ src/main/scala/com/atguigu/scala1128/day06/implicitdemo/ImplicitDemo1.scala
@@ -0,0 +1,16 @@
+package com.atguigu.scala1128.day06.implicitdemo
+
+/**
+ * Author atguigu
+ * Date 2020/4/27 9:08
+ */
+object ImplicitDemo1 {
+    
+    def main(args: Array[String]): Unit = {
+        // 需要一个动作把double转成int, 然后再把值赋值给a
+        implicit def double2Int(d: Double): Int = d.toInt
+        
+        val a: Int = 10.1 // Double=> Int
+        println(a)
+    }
+}
